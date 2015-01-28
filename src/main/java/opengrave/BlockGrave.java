d039c73618d87d7aa7791ecf1425c88ecc71c425
Yopu <yopu42@gmail.com>
2015-01-28 18:30:49 -0500

    Fixed players able to get the grave block

--- src/main/java/opengrave/BlockGrave.java
+++ src/main/java/opengrave/BlockGrave.java
@@ -6,11 +6,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
+
+import java.util.Random;
 
 public class BlockGrave extends BlockContainer {
 
@@ -47,6 +52,16 @@ public class BlockGrave extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World world, int i) {
         return new TileEntityGrave();
+    }
+
+    @Override
+    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_) {
+        return null;
+    }
+
+    @Override
+    public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y, int z) {
+        return null;
     }
 
     @Override
