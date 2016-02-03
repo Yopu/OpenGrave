8b885d1fbc8cb3299467c8593ab01f6405a19e53
yopu <yopu42@gmail.com>
2016-02-02 21:36:27 -0500

    Updated forge, added ssp gradle tasks

--- src/main/kotlin/opengrave/DeathHandler.kt
+++ src/main/kotlin/opengrave/DeathHandler.kt
@@ -7,7 +7,6 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.BlockPos
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.IChatComponent
-import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import net.minecraftforge.event.entity.player.PlayerDropsEvent
 import net.minecraftforge.fluids.IFluidBlock
